2b2ae1c9f7d218b1d01cc30d7ea2ad7053421f6a
Amadd11 <muhammad.syafei.ft21@mail.umy.ac.id>
2023-10-03 14:38:01 +0700

    done

---
+++ app/src/main/java/com/example/test/linear_layoutt.kt
@@ -0,0 +1,11 @@
+package com.example.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class linear_layoutt : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layoutt)
+    }
+}
